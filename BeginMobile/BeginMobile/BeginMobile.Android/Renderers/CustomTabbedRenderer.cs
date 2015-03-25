ea5624e3534b403a16a2e1188c84d2e1542e4e2b
hernan-aguila-globalinsight <hernan.aguila@globalinsightsoft.com>
2015-03-25 12:29:24 -0400

    Update counter icon in home page for message and notification tab

--- BeginMobile/BeginMobile/BeginMobile.Android/Renderers/CustomTabbedRenderer.cs
+++ BeginMobile/BeginMobile/BeginMobile.Android/Renderers/CustomTabbedRenderer.cs
@@ -22,7 +22,7 @@ namespace BeginMobile.Android.Renderers
 
         public CustomTabbedRenderer()
         {
-            this.SetWillNotDraw(false);
+            //this.SetWillNotDraw(false);
         }
 
         protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -31,18 +31,45 @@ namespace BeginMobile.Android.Renderers
             _activity = this.Context as Activity;
         }
 
-        public override void Draw(global::Android.Graphics.Canvas canvas)
+        protected override void OnDraw(global::Android.Graphics.Canvas canvas)
         {
-            base.Draw(canvas);
+            base.OnDraw(canvas);
             var appHome = (AppHome)this.Element;
-            
-            
-            
-
             ActionBar actionBar = _activity.ActionBar;
 
-            actionBar.NavigationMode = ActionBarNavigationMode.Tabs;
-                
+            SetUpTabs(actionBar, appHome);
+        }
+
+        //protected override void DispatchDraw(global::Android.Graphics.Canvas canvas)
+        //{
+        //    base.DispatchDraw(canvas);
+        //    var appHome = (AppHome)this.Element;
+        //    ActionBar actionBar = _activity.ActionBar;
+
+        //    SetUpTabs(actionBar, appHome);
+        //}
+
+        //protected override void OnWindowVisibilityChanged(ViewStates visibility)
+        //{
+        //    base.OnWindowVisibilityChanged(visibility);
+        //    var appHome = (AppHome)this.Element;
+        //    ActionBar actionBar = _activity.ActionBar;
+
+        //    SetUpTabs(actionBar, appHome);
+        //}
+
+
+        # region "Helper methods"
+        private bool TabIsEmpty(ActionBar.Tab tab)
+        {
+            if (tab != null)
+                if (tab.CustomView == null)
+                    return true;
+            return false;
+        }
+
+        private void SetUpTabs(ActionBar actionBar, AppHome appHome)
+        {
             if (actionBar.TabCount > 0)
             {
                 var count = 0;
@@ -62,19 +89,54 @@ namespace BeginMobile.Android.Renderers
 
                         if (childTab is MessageListPage)
                         {
-                            var tabMessage = (MessageListPage) childTab;
-                            var counter = int.Parse(tabMessage.CounterText.Text);
+                            var tabMessage = (MessageListPage)childTab;
+                            int counter;
 
-                            tabBadge.Text = counter > 9 ?
-                                tabMessage.CounterText.Text + "+" : " " + tabMessage.CounterText.Text + " ";
-                        } 
-                        else if(childTab is Pages.Notifications.Notification)
+                            if (int.TryParse(tabMessage.CounterText.Text, out counter))
+                            {
+                                if (counter > 0)
+                                {
+                                    tabBadge.Text = counter > 9 ?
+                                        tabMessage.CounterText.Text + "+" : " " + tabMessage.CounterText.Text + " ";
+
+                                    tabTextAux.Gravity = GravityFlags.Bottom | GravityFlags.Left;
+                                }
+                                else
+                                {
+                                    tabBadge.Visibility = ViewStates.Invisible;
+                                }
+                            }
+                            else
+                            {
+                                tabBadge.Visibility = ViewStates.Invisible;
+                            }
+
+
+                        }
+                        else if (childTab is Pages.Notifications.Notification)
                         {
                             var tabNotification = (Pages.Notifications.Notification)childTab;
-                            var counter = int.Parse(tabNotification.CounterText.Text);
+                            int counter;
 
-                            tabBadge.Text = counter > 9 ?
-                                tabNotification.CounterText.Text + "+" : " "+tabNotification.CounterText.Text+" ";
+                            if (int.TryParse(tabNotification.CounterText.Text, out counter))
+                            {
+                                if (counter > 0)
+                                {
+                                    tabBadge.Text = counter > 9
+                                        ? tabNotification.CounterText.Text + "+"
+                                        : " " + tabNotification.CounterText.Text + " ";
+
+                                    tabTextAux.Gravity = GravityFlags.Bottom | GravityFlags.Left;
+                                }
+                                else
+                                {
+                                    tabBadge.Visibility = ViewStates.Invisible;
+                                }
+                            }
+                            else
+                            {
+                                tabBadge.Visibility = ViewStates.Invisible;
+                            }
                         }
                         else
                         {
@@ -85,42 +147,7 @@ namespace BeginMobile.Android.Renderers
                     count++;
                 }
             }
-         }
-
-        private bool TabIsEmpty(ActionBar.Tab tab)
-        {
-            if (tab != null)
-                if (tab.CustomView == null)
-                    return true;
-            return false;
         }
-
-        /*protected override void OnWindowVisibilityChanged(ViewStates visibility)
-        {
-            base.OnWindowVisibilityChanged(visibility);
-            ActionBar actionBar = _activity.ActionBar;
-
-            actionBar.NavigationMode = ActionBarNavigationMode.Tabs;
-
-            if (actionBar.TabCount > 0)
-            {
-                var count = 0;
-                while (count < actionBar.TabCount)
-                {
-                    ActionBar.Tab tabOne = actionBar.GetTabAt(count);
-
-                    var imageIcon = count == (actionBar.TabCount - 1)
-                        ? Resource.Drawable.menunav
-                        : Resource.Drawable.padlock;
-
-                    tabOne.SetIcon(imageIcon);
-
-                    //padlock
-                    count++;
-                }
-            }
-        }*/
-
-        
+        #endregion
     }
 }
