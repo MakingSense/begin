3ac58b1656b42dae38aedff82a33679d6c269f47
roryvidalburgoa <1x!=+8@K_77\1{E>
2015-03-27 17:51:42 -0400

    Add makingSense logotype.

--- BeginMobile/BeginMobile/BeginMobile.Android/Resources/Resource.Designer.cs
+++ BeginMobile/BeginMobile/BeginMobile.Android/Resources/Resource.Designer.cs
@@ -54,28 +54,31 @@ namespace BeginMobile.Android
 			public const int Icon = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int menunav = 2130837507;
+			public const int logotype = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int monoandroidsplash = 2130837508;
+			public const int menunav = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int padlock = 2130837509;
+			public const int monoandroidsplash = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int ratingoff = 2130837510;
+			public const int padlock = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int ratingon = 2130837511;
+			public const int ratingoff = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int userdefault = 2130837512;
+			public const int ratingon = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int userdefault3 = 2130837513;
+			public const int userdefault = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int userprofile = 2130837514;
+			public const int userdefault3 = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int userprofile = 2130837515;
 			
 			static Drawable()
 			{
