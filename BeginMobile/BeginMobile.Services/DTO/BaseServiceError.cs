0521f329d9fbc755d8784a62eb49548ccadc06ae
hernan-aguila-globalinsight <hernan.aguila@globalinsightsoft.com>
2015-04-10 18:02:53 -0400

    Update BaseServiceError add flag has error to validate if it has errors or a error in the service

--- BeginMobile/BeginMobile.Services/DTO/BaseServiceError.cs
+++ BeginMobile/BeginMobile.Services/DTO/BaseServiceError.cs
@@ -14,5 +14,23 @@ namespace BeginMobile.Services.DTO
 
         [JsonProperty("error")]
         public string Error { set; get; }
+
+        private bool _hasError;
+        public bool HasError
+        {
+            get
+            {
+                if (Errors != null)
+                {
+                    _hasError = Errors.Count > 0;
+                }
+                else
+                {
+                    _hasError = !string.IsNullOrEmpty(Error);
+                }
+
+                return _hasError;
+            }
+        }
     }
 }
