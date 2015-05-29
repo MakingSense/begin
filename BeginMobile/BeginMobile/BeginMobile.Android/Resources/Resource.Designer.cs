e326d2f5760e75af1c9057ae2557d11cf3973ceb
hernan-aguila-globalinsight <hernan.aguila@globalinsightsoft.com>
2015-05-29 19:00:54 -0400

    Add icons and images for Android and IOs

--- BeginMobile/BeginMobile/BeginMobile.Android/Resources/Resource.Designer.cs
+++ BeginMobile/BeginMobile/BeginMobile.Android/Resources/Resource.Designer.cs
@@ -223,169 +223,286 @@ namespace BeginMobile.Android
 			public const int contact_added2 = 2130837522;
 			
 			// aapt resource value: 0x7f020013
-			public const int contacts_menu = 2130837523;
+			public const int contacts_active = 2130837523;
 			
 			// aapt resource value: 0x7f020014
-			public const int CustomBorderEditText = 2130837524;
+			public const int contacts_inactive = 2130837524;
 			
 			// aapt resource value: 0x7f020015
-			public const int events_menu = 2130837525;
+			public const int contacts_menu = 2130837525;
 			
 			// aapt resource value: 0x7f020016
-			public const int greencircle = 2130837526;
+			public const int CustomBorderEditText = 2130837526;
 			
 			// aapt resource value: 0x7f020017
-			public const int groups = 2130837527;
+			public const int edit_active = 2130837527;
 			
 			// aapt resource value: 0x7f020018
-			public const int groups_menu = 2130837528;
+			public const int edit_inactive = 2130837528;
 			
 			// aapt resource value: 0x7f020019
-			public const int Icon = 2130837529;
+			public const int events_active = 2130837529;
 			
 			// aapt resource value: 0x7f02001a
-			public const int iconcontactsactive = 2130837530;
+			public const int events_inactive = 2130837530;
 			
 			// aapt resource value: 0x7f02001b
-			public const int iconcontactsinactive = 2130837531;
+			public const int events_menu = 2130837531;
 			
 			// aapt resource value: 0x7f02001c
-			public const int iconmenuactive = 2130837532;
+			public const int forsteps = 2130837532;
 			
 			// aapt resource value: 0x7f02001d
-			public const int iconmenuinactive = 2130837533;
+			public const int forsteps_activity_active = 2130837533;
 			
 			// aapt resource value: 0x7f02001e
-			public const int iconmessagesactive = 2130837534;
+			public const int forsteps_activity_inactive = 2130837534;
 			
 			// aapt resource value: 0x7f02001f
-			public const int iconmessagesinactive = 2130837535;
+			public const int forsteps_contacts_active = 2130837535;
 			
 			// aapt resource value: 0x7f020020
-			public const int iconnotificationsactive = 2130837536;
+			public const int forsteps_contacts_inactive = 2130837536;
 			
 			// aapt resource value: 0x7f020021
-			public const int iconnotificationsinactive = 2130837537;
+			public const int forsteps_events_active = 2130837537;
 			
 			// aapt resource value: 0x7f020022
-			public const int iconwallactive = 2130837538;
+			public const int forsteps_events_inactive = 2130837538;
 			
 			// aapt resource value: 0x7f020023
-			public const int iconwallinactive = 2130837539;
+			public const int forsteps_groups_active = 2130837539;
 			
 			// aapt resource value: 0x7f020024
-			public const int ievents = 2130837540;
+			public const int forsteps_groups_inactive = 2130837540;
 			
 			// aapt resource value: 0x7f020025
-			public const int ifeed = 2130837541;
+			public const int forsteps_information_active = 2130837541;
 			
 			// aapt resource value: 0x7f020026
-			public const int ifeeds = 2130837542;
+			public const int forsteps_information_inactive = 2130837542;
 			
 			// aapt resource value: 0x7f020027
-			public const int igroup = 2130837543;
+			public const int forsteps_mail_active = 2130837543;
 			
 			// aapt resource value: 0x7f020028
-			public const int iphone_message = 2130837544;
+			public const int forsteps_mail_inactive = 2130837544;
 			
 			// aapt resource value: 0x7f020029
-			public const int logo = 2130837545;
+			public const int forsteps_media_active = 2130837545;
 			
 			// aapt resource value: 0x7f02002a
-			public const int logotype = 2130837546;
+			public const int forsteps_media_inactive = 2130837546;
 			
 			// aapt resource value: 0x7f02002b
-			public const int logout = 2130837547;
+			public const int forsteps_notification_active = 2130837547;
 			
 			// aapt resource value: 0x7f02002c
-			public const int mail = 2130837548;
+			public const int forsteps_notification_inactive = 2130837548;
 			
 			// aapt resource value: 0x7f02002d
-			public const int menunav = 2130837549;
+			public const int forsteps_services_active = 2130837549;
 			
 			// aapt resource value: 0x7f02002e
-			public const int monoandroidsplash = 2130837550;
+			public const int forsteps_services_inactive = 2130837550;
 			
 			// aapt resource value: 0x7f02002f
-			public const int netcomputers = 2130837551;
+			public const int forsteps_setting_active = 2130837551;
 			
 			// aapt resource value: 0x7f020030
-			public const int offline_icon = 2130837552;
+			public const int forsteps_setting_inactive = 2130837552;
 			
 			// aapt resource value: 0x7f020031
-			public const int online_icon = 2130837553;
+			public const int forsteps_shop_active = 2130837553;
 			
 			// aapt resource value: 0x7f020032
-			public const int padlock = 2130837554;
+			public const int forsteps_shop_inactive = 2130837554;
 			
 			// aapt resource value: 0x7f020033
-			public const int photo = 2130837555;
+			public const int greencircle = 2130837555;
 			
 			// aapt resource value: 0x7f020034
-			public const int privacy = 2130837556;
+			public const int groups = 2130837556;
 			
 			// aapt resource value: 0x7f020035
-			public const int ratingoff = 2130837557;
+			public const int groups_active = 2130837557;
 			
 			// aapt resource value: 0x7f020036
-			public const int ratingon = 2130837558;
+			public const int groups_inactive = 2130837558;
 			
 			// aapt resource value: 0x7f020037
-			public const int search = 2130837559;
+			public const int groups_menu = 2130837559;
 			
 			// aapt resource value: 0x7f020038
-			public const int services = 2130837560;
+			public const int hdpi = 2130837560;
 			
 			// aapt resource value: 0x7f020039
-			public const int services_menu = 2130837561;
+			public const int Icon = 2130837561;
 			
 			// aapt resource value: 0x7f02003a
-			public const int shop = 2130837562;
+			public const int iconcontactsactive = 2130837562;
 			
 			// aapt resource value: 0x7f02003b
-			public const int shops_menu = 2130837563;
+			public const int iconcontactsinactive = 2130837563;
 			
 			// aapt resource value: 0x7f02003c
-			public const int splash = 2130837564;
+			public const int iconmenuactive = 2130837564;
 			
 			// aapt resource value: 0x7f02003d
-			public const int splash_screen = 2130837565;
+			public const int iconmenuinactive = 2130837565;
 			
 			// aapt resource value: 0x7f02003e
-			public const int star_active = 2130837566;
+			public const int iconmessagesactive = 2130837566;
 			
 			// aapt resource value: 0x7f02003f
-			public const int star_inactive = 2130837567;
+			public const int iconmessagesinactive = 2130837567;
 			
 			// aapt resource value: 0x7f020040
-			public const int tab_main_selector = 2130837568;
+			public const int iconnotificationsactive = 2130837568;
 			
 			// aapt resource value: 0x7f020041
-			public const int TabBackgroundFocused = 2130837569;
+			public const int iconnotificationsinactive = 2130837569;
 			
 			// aapt resource value: 0x7f020042
-			public const int TabBackgroundNormal = 2130837570;
+			public const int iconwallactive = 2130837570;
 			
 			// aapt resource value: 0x7f020043
-			public const int TabBackgroundSelected = 2130837571;
+			public const int iconwallinactive = 2130837571;
 			
 			// aapt resource value: 0x7f020044
-			public const int takephoto = 2130837572;
+			public const int ievents = 2130837572;
 			
 			// aapt resource value: 0x7f020045
-			public const int terms_conditions = 2130837573;
+			public const int ifeed = 2130837573;
 			
 			// aapt resource value: 0x7f020046
-			public const int userdefault = 2130837574;
+			public const int ifeeds = 2130837574;
 			
 			// aapt resource value: 0x7f020047
-			public const int userdefault3 = 2130837575;
+			public const int igroup = 2130837575;
 			
 			// aapt resource value: 0x7f020048
-			public const int userprofile = 2130837576;
+			public const int iphone_message = 2130837576;
 			
 			// aapt resource value: 0x7f020049
-			public const int write = 2130837577;
+			public const int logo = 2130837577;
+			
+			// aapt resource value: 0x7f02004a
+			public const int logotype = 2130837578;
+			
+			// aapt resource value: 0x7f02004b
+			public const int logout = 2130837579;
+			
+			// aapt resource value: 0x7f02004c
+			public const int mail = 2130837580;
+			
+			// aapt resource value: 0x7f02004d
+			public const int menunav = 2130837581;
+			
+			// aapt resource value: 0x7f02004e
+			public const int monoandroidsplash = 2130837582;
+			
+			// aapt resource value: 0x7f02004f
+			public const int netcomputers = 2130837583;
+			
+			// aapt resource value: 0x7f020050
+			public const int offline_icon = 2130837584;
+			
+			// aapt resource value: 0x7f020051
+			public const int online_icon = 2130837585;
+			
+			// aapt resource value: 0x7f020052
+			public const int padlock = 2130837586;
+			
+			// aapt resource value: 0x7f020053
+			public const int photo = 2130837587;
+			
+			// aapt resource value: 0x7f020054
+			public const int privacy = 2130837588;
+			
+			// aapt resource value: 0x7f020055
+			public const int ratingoff = 2130837589;
+			
+			// aapt resource value: 0x7f020056
+			public const int ratingon = 2130837590;
+			
+			// aapt resource value: 0x7f020057
+			public const int search = 2130837591;
+			
+			// aapt resource value: 0x7f020058
+			public const int search_active = 2130837592;
+			
+			// aapt resource value: 0x7f020059
+			public const int search_inactive = 2130837593;
+			
+			// aapt resource value: 0x7f02005a
+			public const int sendIcon = 2130837594;
+			
+			// aapt resource value: 0x7f02005b
+			public const int services = 2130837595;
+			
+			// aapt resource value: 0x7f02005c
+			public const int services_active = 2130837596;
+			
+			// aapt resource value: 0x7f02005d
+			public const int services_inactive = 2130837597;
+			
+			// aapt resource value: 0x7f02005e
+			public const int services_menu = 2130837598;
+			
+			// aapt resource value: 0x7f02005f
+			public const int shop = 2130837599;
+			
+			// aapt resource value: 0x7f020060
+			public const int shop_active = 2130837600;
+			
+			// aapt resource value: 0x7f020061
+			public const int shop_inactive = 2130837601;
+			
+			// aapt resource value: 0x7f020062
+			public const int shops_menu = 2130837602;
+			
+			// aapt resource value: 0x7f020063
+			public const int splash = 2130837603;
+			
+			// aapt resource value: 0x7f020064
+			public const int splash_screen = 2130837604;
+			
+			// aapt resource value: 0x7f020065
+			public const int star_active = 2130837605;
+			
+			// aapt resource value: 0x7f020066
+			public const int star_inactive = 2130837606;
+			
+			// aapt resource value: 0x7f020067
+			public const int tab_main_selector = 2130837607;
+			
+			// aapt resource value: 0x7f020068
+			public const int TabBackgroundFocused = 2130837608;
+			
+			// aapt resource value: 0x7f020069
+			public const int TabBackgroundNormal = 2130837609;
+			
+			// aapt resource value: 0x7f02006a
+			public const int TabBackgroundSelected = 2130837610;
+			
+			// aapt resource value: 0x7f02006b
+			public const int takephoto = 2130837611;
+			
+			// aapt resource value: 0x7f02006c
+			public const int terms_conditions = 2130837612;
+			
+			// aapt resource value: 0x7f02006d
+			public const int userdefault = 2130837613;
+			
+			// aapt resource value: 0x7f02006e
+			public const int userdefault3 = 2130837614;
+			
+			// aapt resource value: 0x7f02006f
+			public const int userprofile = 2130837615;
+			
+			// aapt resource value: 0x7f020070
+			public const int write = 2130837616;
 			
 			static Drawable()
 			{
